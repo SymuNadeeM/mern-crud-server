c77b0d09a585750d52edf872a984cc2c71864b00
SymuNadeem <msymunnadeem@gmail.com>
2024-07-08 22:27:34 +0600

    address modal and create set

--- index.js
+++ index.js
@@ -1,64 +1,93 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 //schema
-const schemaData = mongoose.Schema({
-  name: String,
-  email: String,
-  mobile: String
-},{
-  timestamps: true
-})
+const schemaData = mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    mobile: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userModal = mongoose.model("user",schemaData)
+const userModal = mongoose.model("user", schemaData);
 
-//read 
-app.get("/",async(req,res)=>{
-  const data = await userModal.find({})
-  res.json({success:true, data : data})
-})
+// for address schema -----------------------------------------
+const addressSchemaData = mongoose.Schema(
+  {
+    address: String,
+    blood_group: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userAddressModal = mongoose.modelNames("address", addressSchemaData);
+
+app.get("/all-address", async (req, res) => {
+  const data = await userAddressModal.find({});
+  res.json({ success: true, data: data });
+});
+
+app.post("/create-address", async (req, res) => {
+  const data = new userAddressModal(req.body);
+  await data.save();
+  res.send({ success: true, message: "save data successfully", data: data });
+});
+
+// for address schema End -----------------------------------------
+
+//read
+app.get("/", async (req, res) => {
+  const data = await userModal.find({});
+  res.json({ success: true, data: data });
+});
 
 //create data
-app.post("/create", async(req,res)=>{
+app.post("/create", async (req, res) => {
   console.log(req.body);
-  const data = new userModal(req.body)
-  await data.save()
-  res.send({success : true, message : "save data successfully",data : data})
-})
-
+  const data = new userModal(req.body);
+  await data.save();
+  res.send({ success: true, message: "save data successfully", data: data });
+});
 
 //update data
-app.put("/update",async(req,res)=>{
+app.put("/update", async (req, res) => {
   console.log(req.body);
-  const {id,...rest} = req.body
+  const { id, ...rest } = req.body;
   console.log(rest);
-  const data = await userModal.updateOne({_id: id},rest)
-  res.send({success : true, message : "save update successfully",data : data})
-})
+  const data = await userModal.updateOne({ _id: id }, rest);
+  res.send({ success: true, message: "save update successfully", data: data });
+});
 
 //Delete
-app.delete("/delete/:id", async(req,res)=>{
-  const id = req.params.id
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
   console.log(id);
-  const data = await userModal.deleteOne({_id: id})
-  res.send({success : true, message : "save delete successfully",data : data})
-})
+  const data = await userModal.deleteOne({ _id: id });
+  res.send({ success: true, message: "save delete successfully", data: data });
+});
 
 // app.get("/",(req,res)=>{
 //   res.json({message: "server crud running"})
 // })
 
-mongoose.connect(process.env.MONOGO_URL)
-.then(()=>{
-  console.log("connect to DB")
-  app.listen(PORT,()=>console.log("server is running"))
-})
-.catch((err)=>console.log(err))
+mongoose
+  .connect(process.env.MONOGO_URL)
+  .then(() => {
+    console.log("connect to DB");
+    app.listen(PORT, () => console.log("server is running"));
+  })
+  .catch((err) => console.log(err));
